a966d3a5e36b0ca58d584659aaa4b8992bee198b
rlmarasigan <raphaellemarasigan@gmail.com>
2021-11-24 22:32:22 +0800

    Updated constants

---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const kTitleTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kWhite = Color(0xccffffff);
+
+const kGray = Color(0xff292929);
+
+const kBlack = Color(0xff121212);
+
+const kBlue = Color(0xffb102A43);
+
+const kGreen = Color(0xff2abfa4);
+
+const kRed = Color(0xffdb4d4d);
