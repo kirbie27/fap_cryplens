04eca0d174d23a117be5700e00f7aaa3f6303790
Kirby Wenceslao <joankirby.wenceslao.iics@ust.edu.ph>
2021-11-29 15:47:34 +0800

    pushed search.dart

---
+++ lib/search.dart
@@ -0,0 +1,15 @@
+class Search {
+  static String _search = "";
+
+  setSearch(String text) {
+    _search = text;
+  }
+
+  getSearch() {
+    return _search;
+  }
+
+  resetSearch() {
+    _search = "";
+  }
+}
