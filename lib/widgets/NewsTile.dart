7c1716d316a3d77cb68f574b8e6a9f9df93504a6
rlmarasigan <raphaellemarasigan@gmail.com>
2021-12-01 22:16:55 +0800

    cleaned NewsTile widget

--- lib/widgets/NewsTile.dart
+++ lib/widgets/NewsTile.dart
@@ -2,19 +2,10 @@ import 'package:cryplens/constants.dart';
 import 'package:cryplens/screens/article_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
-
 import 'package:flutter/rendering.dart';
 
 class NewsTile extends StatelessWidget {
   final String imageUrl, title, desc, url;
-
-  double computeHeight(int desc, int title) {
-    //print('length $desc');
-    //dynamically computes the better fit height for each news article.
-    return (50 + 15 * (desc / 50.0).ceil() + 15 * (title / 30.0).ceil())
-        .toDouble();
-  }
 
   NewsTile(
       {required this.imageUrl,
@@ -24,6 +15,7 @@ class NewsTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //GestureDetector is used to select the whole news tile to access the webview articles
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -31,6 +23,7 @@ class NewsTile extends StatelessWidget {
             MaterialPageRoute(
                 builder: (context) => ArticleView(articleURL: url)));
       },
+      //This container contains the apis image network which serves as the background of the news tile
       child: Container(
         decoration: BoxDecoration(
           color: kBlue,
@@ -41,27 +34,19 @@ class NewsTile extends StatelessWidget {
               colorFilter: new ColorFilter.mode(
                   kBlue.withOpacity(0.3), BlendMode.dstATop)),
         ),
-        // height: computeHeight(desc.length, title.length),
         height: 150,
         padding: EdgeInsets.symmetric(horizontal: 30),
         margin: EdgeInsets.fromLTRB(15, 10, 15, 16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            //SizedBox(height: 8),
-            // ClipRRect(
-            //     borderRadius: BorderRadius.circular(10.0),
-            //     child: Image.network(
-            //       imageUrl,
-            //       fit: BoxFit.fill,
-            //       height: 170,
-            //     )),
-            // SizedBox(height: 8),
             Text(
               title,
               style: kArticleTitleTextStyle,
             ),
             SizedBox(height: 8),
+            //Flexible widget is used for the description of each articles which also came from the json file of the api
+            //The ellipsis property is used to prevent overflow formatting
             Flexible(
               child: Text(
                 desc,
@@ -70,21 +55,6 @@ class NewsTile extends StatelessWidget {
                 overflow: TextOverflow.ellipsis,
               ),
             ),
-            // ClipRRect(
-            //     borderRadius: BorderRadius.circular(30.0),
-            //     child: Container(
-            //       width: 250,
-            //       height: 30,
-            //       color: kWhite,
-            //       child: Container(
-            //         margin: EdgeInsets.only(top: 7),
-            //         child: Text(
-            //           'READ FULL ARTICLE',
-            //           textAlign: TextAlign.center,
-            //         ),
-            //       ),
-            //     )),
-            // SizedBox(height: 10),
           ],
         ),
       ),
